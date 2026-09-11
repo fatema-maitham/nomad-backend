9471dd63e3789f5c3ef46054c02889596f49e335
fatema-maitham <fatemamaitham1@gmail.com>
2026-09-11 14:31:14 +0300

    delete user profile

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -38,7 +38,23 @@ const updateUser = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.user._id);
+
+    if (!user) {
+      return res.status(404).json({ err: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ err: err.message });
+  }
+};
+
 module.exports = {
   getUser,
   updateUser,
+  deleteUser
 };
