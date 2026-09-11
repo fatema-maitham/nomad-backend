7e5775f157e6580229f0cdde6ebf0f15f728bcdf
fatema-maitham <fatemamaitham1@gmail.com>
2026-09-11 15:20:09 +0300

    complete products CRUD

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -11,6 +11,80 @@ const getProducts = async (req, res) => {
   }
 };
 
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    res.status(201).json(product);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ err: err.message });
+  }
+};
+
+const showProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId).populate(
+      'category'
+    );
+
+    if (!product) {
+      return res.status(404).json({ err: 'Product not found' });
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ err: err.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.productId,
+      {
+        ref: req.body.ref,
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        stock: req.body.stock,
+        pictures: req.body.pictures,
+        category: req.body.category,
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!product) {
+      return res.status(404).json({ err: 'Product not found' });
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ err: err.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.productId);
+
+    if (!product) {
+      return res.status(404).json({ err: 'Product not found' });
+    }
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ err: err.message });
+  }
+};
+
 module.exports = {
   getProducts,
+  createProduct,
+  showProduct,
+  updateProduct,
+  deleteProduct,
 };
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,5 +5,9 @@ const productCtrl = require('../controllers/productCtrl');
 const router = express.Router();
 
 router.get('/', productCtrl.getProducts);
+router.post('/', productCtrl.createProduct);
+router.get('/:productId', productCtrl.showProduct);
+router.put('/:productId', productCtrl.updateProduct);
+router.delete('/:productId', productCtrl.deleteProduct);
 
 module.exports = router;
