f2fa09cc3c5f70433cfe9d0abc63826142d27885
fatema-maitham <fatemamaitham1@gmail.com>
2026-09-11 14:26:38 +0300

    edit user profile

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -15,6 +15,30 @@ const getUser = async (req, res) => {
   }
 };
 
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        name: req.body.name,
+        email: req.body.email,
+        contact: req.body.contact,
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ err: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ err: err.message });
+  }
+};
+
 module.exports = {
   getUser,
+  updateUser,
 };
